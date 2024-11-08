b34159dff1491a823b34b95e9ca1c1c2ef62319a
0xsufi <0xsufi@noma.money>
2024-11-08 17:10:05 -0400

    use crypto

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cors = require("cors"); 
+const crypto = require('crypto')
 
 const app = express();
 const PORT = 3000;
